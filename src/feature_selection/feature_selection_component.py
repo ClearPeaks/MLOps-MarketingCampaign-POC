42fee49058985cb7ed75d8506f96a287740b5f78
Alejandro Donaire <alejandro.donaire@clearpeaks.com>
2023-11-28 09:24:41 +0000

    Test change in component

--- src/feature_selection/feature_selection_component.py
+++ src/feature_selection/feature_selection_component.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 @command_component(
     name="feature_selection",
-    version="1",
+    # version="1",
     display_name="Feature Selection",
     description="Selects the relevant features for the model.",
     environment=dict(
@@ -26,7 +26,7 @@ def feature_selection_component(
     remove_list = ['ID', 'Year_Birth', 'NumDealsPurchases', 'NumStorePurchases',
                    'NumWebVisitsMonth', 'Complain', 'Z_CostContact', 'Z_Revenue']
     
-    # Deleting the previously selected variables from the dataset
+    # Deleting previous list of variables from the dataset
     data_selected = data.drop(remove_list, axis=1)
 
     # Saving to output path
